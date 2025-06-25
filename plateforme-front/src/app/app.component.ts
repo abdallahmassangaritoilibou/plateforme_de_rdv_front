a66f9437dae7e405f31dd16935d90aec027b43b6
abdallahmassangaritoilibou <abdallah.toilbou@laposte.net>
2025-06-25 17:26:40 +0200

    header avec le nom du site

--- plateforme-front/src/app/app.component.ts
+++ plateforme-front/src/app/app.component.ts
@@ -1,23 +1,13 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <div class="app-container">
-      <router-outlet></router-outlet>
-    </div>
-  `,
-  styles: [`
-    .app-container {
-      min-height: 100vh;
-      background-color: #f5f5f5;
-      padding: 20px;
-    }
-  `]
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mon-rdv-app';
-}
+export class AppComponent {}
---
+++ plateforme-front/src/app/shared/header/header.component.ts
@@ -0,0 +1,15 @@
+// src/app/shared/header/header.component.ts
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+
+})
+export class HeaderComponent {
+ appName = "MyRdvLib";
+ 
+}
