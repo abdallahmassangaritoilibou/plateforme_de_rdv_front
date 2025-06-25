6e545de0d185b0949917c5a7f13de2e9dbf3bb78
abdallahmassangaritoilibou <abdallah.toilbou@laposte.net>
2025-06-25 22:19:53 +0200

    l'utilisateur peux ce connecter

--- plateforme-front/src/app/app.routes.ts
+++ plateforme-front/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/register', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
-  
-  // l'autres routes ici
+  { path: 'login', component: LoginComponent },
 ];
