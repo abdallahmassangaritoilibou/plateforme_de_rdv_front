c70eede42db59784e350460ac2d88d56786f30f2
abdallahmassangaritoilibou <abdallah.toilbou@laposte.net>
2025-07-08 19:56:13 +0200

    Ajout d’un bouton retour sur la page Mes rendez-vousEspacement entre les créneaux pour une meilleure lisibilité - Amélioration des styles dans booking et appointments - Mise à jour des services pour prise et annulation de rendez-vous - Nettoyage mineur du header

--- plateforme-front/src/app/my-appointments/my-appointments.component.ts
+++ plateforme-front/src/app/my-appointments/my-appointments.component.ts
@@ -1,14 +1,16 @@
 
 // src/app/my-appointments.component.ts
 import { Component, OnInit } from '@angular/core';
-import { RendezVousService, RendezVous } from '../services/rendezvous.service';
+import { RendezVousService, RendezVousDTO } from '../services/rendezvous.service';
 import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { Router, RouterModule } from '@angular/router';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { StorageService } from '../services/storage.service';
-
+import { switchMap } from 'rxjs';
+import { AvailabilityService } from '../services/availability.service';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-my-appointments',
   standalone: true,
@@ -16,6 +18,7 @@ import { StorageService } from '../services/storage.service';
     CommonModule,
     MatListModule,
     MatCardModule,
+    MatButtonModule,
     RouterModule,
     MatSnackBarModule,
   ],
@@ -24,25 +27,62 @@ import { StorageService } from '../services/storage.service';
   styleUrls: ['./my-appointments.component.css'],
 })
 export class MyAppointmentsComponent implements OnInit {
-  appointments: RendezVous[] = [];
+  appointments: RendezVousDTO[] = [];
 
   constructor(
     private readonly rdvService: RendezVousService,
     private readonly snack: MatSnackBar,
     private readonly router: Router,
-    private readonly storage: StorageService
+    private readonly storage: StorageService,
+    private readonly availService: AvailabilityService
   ) {}
 
   ngOnInit(): void {
     this.rdvService.getMine().subscribe({
-      next: list => this.appointments = list,
+      next: list => {
+       this.appointments = list.filter(rdv => rdv.status !== 'ANNULE')},
       error: err => console.error(err)
     });
   }
+
+  
+  cancel(rdv: RendezVousDTO) {
+  console.log('RDV à annuler:', rdv);
+
+  if (!rdv.availabilityId) {
+    this.snack.open('Ce rendez-vous n’a pas de créneau associé.', '', { duration: 3000 });
+    return;
+  }
+ 
+
+  this.rdvService.cancelRdv(rdv).pipe(
+    switchMap(() => this.availService.updateIsBooked(rdv.availabilityId, false))
+  ).subscribe({
+    next: () => {
+      this.snack.open('RDV annulé, créneau remis dispo.', '', { duration: 2000 });
+      this.rdvService.getMine().subscribe(list => {
+      this.appointments = list.filter(rdv => rdv.status !== 'ANNULE');
+      
+});
+
+    },
+    error: (err: any)=> {
+      console.error('Erreur lors de l’annulation :', err);
+      this.snack.open('Erreur pendant l’annulation.', '', { duration: 3000 });
+    }
+  });
+}
+
+
 
   logout(): void {
     this.storage.remove('userId');
     this.snack.open('Déconnexion réussie', '', { duration: 2000 });
     this.router.navigate(['/login']);
   }
+
+  goToReservation(): void {
+  this.router.navigate(['/booking']);
+}
+
   }
--- plateforme-front/src/main.ts
+++ plateforme-front/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+
+registerLocaleData(localeFr, 'fr'); 
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
